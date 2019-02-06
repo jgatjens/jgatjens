aa77deaf081778acf72327351ff444c9e7a30788
Jairo Gatjens <jgatjens@gmail.com>
2019-02-06 15:55:33 -0600

    Updates

--- static.d6fd8322.js
+++ static.d6fd8322.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "//";
+/******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 11);
@@ -1891,4 +1891,4 @@ module.exports = __webpack_require__.p + "static/share-promo.1de58934.png";
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.f98f85a7.js.map
+//# sourceMappingURL=static.d6fd8322.js.map
